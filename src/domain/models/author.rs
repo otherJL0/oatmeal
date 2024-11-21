8b59363208bbc2157e661fe65db723fb38686f61
Jonathan Lopez <jonathanglopez@gmail.com>
2024-11-21 16:37:40 -0500

    Use Display over to_string

--- src/domain/models/author.rs
+++ src/domain/models/author.rs
@@ -11,12 +11,13 @@ pub enum Author {
     Model,
 }
 
-impl ToString for Author {
-    fn to_string(&self) -> String {
-        match self {
-            Author::User => return Config::get(ConfigKey::Username),
-            Author::Oatmeal => return String::from("Oatmeal"),
-            Author::Model => return Config::get(ConfigKey::Model),
-        }
+impl std::fmt::Display for Author {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        let output = match self {
+            Author::User => Config::get(ConfigKey::Username),
+            Author::Oatmeal => String::from("Oatmeal"),
+            Author::Model => Config::get(ConfigKey::Model),
+        };
+        return write!(f, "{}", output);
     }
 }
